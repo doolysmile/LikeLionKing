d32f1d1bee32f99cac6b32aa86e59dff4828cb2a
KCH <doolysmile1@naver.com>
2022-07-19 23:43:02 +0900

    feat : post 추가시 postDto 반환

--- src/main/java/com/kch/likelion/LikeLionKing/post/controller/PostController.java
+++ src/main/java/com/kch/likelion/LikeLionKing/post/controller/PostController.java
@@ -3,6 +3,7 @@ package com.kch.likelion.LikeLionKing.post.controller;
 
 
 import com.kch.likelion.LikeLionKing.post.domain.Post;
+import com.kch.likelion.LikeLionKing.post.domain.PostDto;
 import com.kch.likelion.LikeLionKing.post.domain.PostRequestDto;
 import com.kch.likelion.LikeLionKing.post.domain.PostResponseDto;
 import com.kch.likelion.LikeLionKing.post.service.PostService;
@@ -14,7 +15,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
 
+// TODO : PostResposneDTO 합치는 쪽으로 생각해보기
 @RestController
 @AllArgsConstructor
 @RequestMapping("post")
@@ -23,17 +26,18 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping("write")
-    public ResponseEntity<PostResponseDto> writePost(@RequestBody PostRequestDto postRequest){
+    public ResponseEntity<PostDto> writePost(@RequestBody PostRequestDto postRequest){
 
         System.out.println(postRequest.getContent());
         Post newPost = postService.insert(postRequest.newPost());
         System.out.println(newPost.getPostSeq());
         if(newPost != null){
-            return ResponseEntity.status(HttpStatus.OK).body(new PostResponseDto(true, "등록완료"));
+            return ResponseEntity.status(HttpStatus.OK).body(new PostDto(newPost));
         }
-        return ResponseEntity.status(HttpStatus.OK).body(new PostResponseDto(false, "등록실패"));
+        // TODO 리턴 값 생각 해보기
+        return ResponseEntity.status(HttpStatus.OK).body(null);
     }
-    // TODO : 실패 했을 때 예외 처리 해줘야함.
+
     @PostMapping("update")
     public ResponseEntity<Object> updatePost(@RequestBody PostRequestDto postWriteRequest){
         postService.update(postWriteRequest.newPost());
@@ -50,8 +54,10 @@ public class PostController {
     // TODO : 성공 여부와 객체 리턴을 함꼐 출력 할 수 있도록 만들기.
     @GetMapping("list")
     public ResponseEntity<List<Object>> listPost(){
-        List<Post> all = postService.findAll();
-        return ResponseEntity.status(HttpStatus.OK).body(Arrays.asList(all.toArray()));
+        List<PostDto> postDtoLists= postService.findAll().stream()
+                .map(PostDto::new).collect(toList());
+
+        return ResponseEntity.status(HttpStatus.OK).body(Arrays.asList(postDtoLists.toArray()));
     }
 
 
