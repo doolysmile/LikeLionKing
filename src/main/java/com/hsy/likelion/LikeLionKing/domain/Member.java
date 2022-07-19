0b77cb70ddab6491f8115dfad8e76319c1b377da
ahah525 <ahah525@naver.com>
2022-07-19 21:57:04 +0900

    user, admin 구분을 위한 Member 수정

--- src/main/java/com/hsy/likelion/LikeLionKing/domain/Member.java
+++ src/main/java/com/hsy/likelion/LikeLionKing/domain/Member.java
@@ -1,25 +1,26 @@
 package com.hsy.likelion.LikeLionKing.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class Member {
-    @JsonProperty("id")
     private Long id;        // 회원 id
-    @JsonProperty("login_id")
     private String loginId; // 회원 로그인 id
-    @JsonProperty("login_pw")
     private String loginPw; // 회원 로그인 pw
+    private int role;       // 회원 종류
 
     public Member() {
     }
 
-    public Member(Long id, String loginId, String loginPw) {
+    public Member(Long id, String loginId, String loginPw, int role) {
         this.id = id;
         this.loginId = loginId;
         this.loginPw = loginPw;
+        this.role = role;
     }
 }
