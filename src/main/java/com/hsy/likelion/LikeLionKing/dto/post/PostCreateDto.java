6f91ffd25171b6d7d5b5d7d234a17cce640cf850
ahah525 <ahah525@naver.com>
2022-07-20 16:16:39 +0900

    PostCreatedDto 수정

--- src/main/java/com/hsy/likelion/LikeLionKing/dto/post/PostCreateDto.java
+++ src/main/java/com/hsy/likelion/LikeLionKing/dto/post/PostCreateDto.java
@@ -9,22 +9,41 @@ import lombok.Setter;
 @Setter
 @Builder
 public class PostCreateDto {
-    private Long id;
-    private String title;
-    private String content;
 
-    public PostCreateDto(Long id, String title, String content) {
+    private Long id;        // 게시글 id
+    private Long memberId;  // 작성자 id
+    private Integer categoryId; // 게시글 카테고리 id
+    private String title;   // 게시글 제목
+    private String content; // 게시글 내용
+    private String created; // 게시물 생성 일시
+    private String lastUpdated;    // 게시물 최종 수정 일시
+    private Long views;     // 조회수
+    private Long likes;     // 좋아요 수
+
+    public PostCreateDto(Long id, Long memberId, Integer categoryId, String title, String content, String created, String lastUpdated, Long views, Long likes) {
         this.id = id;
+        this.memberId = memberId;
+        this.categoryId = categoryId;
         this.title = title;
         this.content = content;
+        this.created = created;
+        this.lastUpdated = lastUpdated;
+        this.views = views;
+        this.likes = likes;
     }
 
     // PostCreateDto -> Post 변환
     public static Post toEntity(PostCreateDto postCreateDto) {
         // id는 DB에서 자동 생성되어 부여되기 떄문에 전달X
         Post post = Post.builder()
+                .memberId(postCreateDto.getMemberId())
+                .categoryId(postCreateDto.getCategoryId())
                 .title(postCreateDto.getTitle())
                 .content(postCreateDto.getContent())
+                .created(postCreateDto.getCreated())
+                .lastUpdated(postCreateDto.getLastUpdated())
+                .views(postCreateDto.getViews())
+                .likes(postCreateDto.getLikes())
                 .build();
         return post;
     }
@@ -33,8 +52,14 @@ public class PostCreateDto {
     public static PostCreateDto toDto(Post post) {
         PostCreateDto postCreateDto = PostCreateDto.builder()
                 .id(post.getId())
+                .memberId(post.getMemberId())
+                .categoryId(post.getCategoryId())
                 .title(post.getTitle())
                 .content(post.getContent())
+                .created(post.getCreated())
+                .lastUpdated(post.getLastUpdated())
+                .views(post.getViews())
+                .likes(post.getLikes())
                 .build();
         return postCreateDto;
     }
