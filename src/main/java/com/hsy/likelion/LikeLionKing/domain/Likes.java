b5b18c112dd2c039e48c9f0ab00d882325f90a59
ahah525 <ahah525@naver.com>
2022-08-15 22:30:18 +0900

    feat : 좋아요 기능 구현을 위한 Likes entity, dto 추가

---
+++ src/main/java/com/hsy/likelion/LikeLionKing/domain/Likes.java
@@ -0,0 +1,14 @@
+package com.hsy.likelion.LikeLionKing.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class Likes {
+    private Long id;
+    private Long memberId; // 회원 id
+    private Long postId;   //  게시물 id
+}
